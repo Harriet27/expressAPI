0d6eef31caf86e911ca7274d224af2849d93ea4a
Aldrich Neil <aldrich.neil.hung@gmail.com>
2020-04-01 14:09:30 +0700

    first commit

--- router/userRouter.js
+++ router/userRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { userController } = require('../controller/index');
 
 router.get('/getUsers', userController.getAllUsers);
-router.get('/getById', userController.getUserById);
+router.get('/getById/:id', userController.getUserById);
 router.get('/search-username', userController.searchByUsername);
 router.get('/search-role', userController.searchByRole);
 router.get('/login', userController.login);
