b5fdd9a8acf2a33e2da7c409da014023313bb4c7
Aldrich Neil <aldrich.neil.hung@gmail.com>
2020-04-01 13:55:01 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const port = 2000;
+const bodyParser = require('body-parser');
+app.use(bodyParser());
+
+app.get('/', (req,res) => {
+    res.status(200).send(`<h1>Welcome to my API</h1>`)
+});
+app.post('/post', (req,res) => {
+    console.log(req.body)
+    /*
+    Axios.post(API_URL, data)
+    req.body => ambil data dari frontend
+    req.params => data dari url endpoint
+    req.query => search
+    */
+   if (req.body.username === 'lian') {
+        res.status(200).send('<h1>Lanjut</h1>')
+    } else {
+        res.status(200).send('<h1>Ga Boleh</h1>')
+    }
+    res.status(200).send(`<h1>POST</h1>`)
+});
+app.patch('/patch', (req,res) => {
+    console.log(req.params)
+    res.status(200).send(`<h1>PATCH</h1>`)
+});
+app.put('/put', (req,res) => {
+    console.log(req.query)
+    res.status(200).send(`<h1>PUT</h1>`)
+});
+app.delete('/', (req,res) => {
+    res.status(200).send(`<h1>DELETE</h1>`)
+});
+
+const { userRouter } = require('./router');
+app.use('/users', userRouter);
+
+app.listen(port, () => console.log(`API active at port ${port}`));
---
+++ router/userRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { userController } = require('../controller/index');
+
+router.get('/getUsers', userController.getAllUsers);
+router.get('/getById', userController.getUserById);
+router.get('/search-username', userController.searchByUsername);
+router.get('/search-role', userController.searchByRole);
+router.get('/login', userController.login);
+
+module.exports = router;
